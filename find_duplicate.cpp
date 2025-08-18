8b1841011175d1e00c7d189a4f1e32444533330f
chinmay2975-crypto <chinmay2975@gmail.com>
2025-08-18 18:22:51 +0530

    Create find_duplicate.cpp

---
+++ find_duplicate.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int low=1,high=nums.size()-1;
+        
+       
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            int count = 0;
+             for (int num : nums) {
+        if (num <= mid) 
+        count++;
+}
+            if(count>mid){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
