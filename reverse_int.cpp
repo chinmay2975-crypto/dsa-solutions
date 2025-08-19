2f9bad077bd5f5188297961d794da6ecd143834f
chinmay2975-crypto <chinmay2975@gmail.com>
2025-08-19 15:45:33 +0530

    Create reverse_int.cpp

---
+++ reverse_int.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+   int reverse(int x) {
+    int reversed = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+
+        if (reversed > INT_MAX / 10  ){
+        return 0;
+        }
+         
+         if (reversed < INT_MIN / 10  ){
+        return 0;
+         }
+        reversed = reversed * 10 + digit;
+        x /= 10;
+    }
+
+    return reversed;
+}
+
+};
